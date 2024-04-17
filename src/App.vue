1be3cc550b32cc5b4d22ed4f00fde697bc9f9d7b
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-04-17 12:47:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <AppHeader></AppHeader>
+</template>
+
+<script>
+    import AppHeader from './components/AppHeader.vue';
+    import axios from 'axios';
+
+    export default {
+        components: {
+            AppHeader,
+        }
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+    mounted() {
+
+    },
+    computed: {
+
+    },
+    created() {
+
+    },
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="input-group mb-3">
+        <input type="text" class="form-control" placeholder="Recipient's username" aria-label="Recipient's username" aria-describedby="button-addon2">
+        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Button</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'AppHeader'
+    }
+</script>
+
+<style lang="scss" scoped>
+    @use '../assets/scss/partials/AppHeader.scss' as *;
+</style>
