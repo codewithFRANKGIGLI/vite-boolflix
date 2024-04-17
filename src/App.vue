034e1a7dd71be8c731b24b82e70af84414764897
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-04-17 15:21:48 +0200

    feat: store.js

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,10 @@
 </template>
 
 <script>
+    import { store } from "./store.js";
     import AppHeader from './components/AppHeader.vue';
     import AppMain from './components/AppMain.vue';
+    import axios from 'axios';
 
     export default {
         components: {
@@ -14,9 +16,18 @@
         },
         data() {
             return {
-                
+                store,
             }
-        }
+        },
+        methods: {
+            getDataMoviesFromApi() {
+                axios.get('https://api.themoviedb.org/3/search/movie?api_key=612e3f2fe173487edf095e3b2629965d')
+                .then((response) => {
+                    
+                })
+            }
+        },
+        
     }
 </script>
 
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container p-2">
         <div class="input-group mb-3">
-            <input type="text" class="form-control" placeholder="Search series TV or movies">
+            <input v-model="store.inputSearch" type="text" class="form-control" placeholder="Search series TV or movies">
             <button class="btn btn-warning" type="button" id="button-addon2">Search</button>
         </div>
     </div>
