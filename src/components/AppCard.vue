d7ce565e521f8fd0e14d021828eef9e459beb807
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-04-18 14:27:07 +0200

    feat: milestone 2

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="card text-white" style="width: 20rem;">
-        <img :src="'https://image.tmdb.org/t/p/w342'+ movieInfo.poster_path" class="card-img" alt="...">
+    <div class="card text-white lh-lg myTextShadow" style="width: 20rem;">
+        <img :src="'https://image.tmdb.org/t/p/w342'+ movieInfo.poster_path" class="hoverImgOp card-img" alt="...">
         <div class="hoverOp card-img-overlay">
-            <h5 class="card-title">{{ movieInfo.title }}</h5>
-            <p class="card-text">Titolo Originale: {{ movieInfo.original_title }}</p>
-            <p class="card-text">Lingua originale:
+            <h5 class="fs-4 card-title">{{ movieInfo.title }}</h5>
+            <p class="fs-5 card-text">Titolo Originale: {{ movieInfo.original_title }}</p>
+            <p class="fs-5 card-text">Lingua originale:
                 <img class="w-25 m-2" :src="getImageUrl(movieInfo.original_language)" alt="">
             </p>
-            <p class="card-text">Voto medio: {{ movieInfo.vote_average }}</p>
+            <div class="fs-5 card-text">Voto medio:
+                <i v-show="Math.ceil(movieInfo.vote_average / 2)>=1" class="fa-solid fa-star"></i>
+                <i v-show="Math.ceil(movieInfo.vote_average / 2)>=2"class="fa-solid fa-star"></i>
+                <i v-show="Math.ceil(movieInfo.vote_average / 2)>=3"class="fa-solid fa-star"></i>
+                <i v-show="Math.ceil(movieInfo.vote_average / 2)>=4"class="fa-solid fa-star"></i>
+                <i v-show="Math.ceil(movieInfo.vote_average / 2)>=5"class="fa-solid fa-star"></i>
+            </div>
         </div>
     </div>
 </template>
@@ -21,20 +27,25 @@
         methods: {
             getImageUrl(name) {
                 return new URL(`../assets/img/flags/${name}.png`, import.meta.url).href;
-            },
+            }
         },
     }
 </script>
 
 <style lang="scss" scoped>
+    
     .hoverOp {
         opacity: 0;
         &:hover {
             opacity: 1;
             transition: 1s;
+            background-color: rgba($color: #000000, $alpha: 0.5);
         }
     };
-    h5, p {
-        text-shadow: 1px 1px 1px black;
+    .myTextShadow {
+        text-shadow: 1px 1px 1px red;
+    };
+    i {
+        color: yellow;
     }
 </style>
