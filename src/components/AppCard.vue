0724adb3735d5f09da0d6402487025de11cec783
codewithFRANKGIGLI <gigli.francesco099@gmail.com>
2024-04-18 13:02:33 +0200

    fix: flags

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,10 +1,12 @@
 <template>
-    <div class="card text-white" style="width: 18rem;">
+    <div class="card text-white" style="width: 20rem;">
         <img :src="'https://image.tmdb.org/t/p/w342'+ movieInfo.poster_path" class="card-img" alt="...">
         <div class="hoverOp card-img-overlay">
             <h5 class="card-title">{{ movieInfo.title }}</h5>
             <p class="card-text">Titolo Originale: {{ movieInfo.original_title }}</p>
-            <p class="card-text">Lingua originale: {{ movieInfo.original_language }}</p>
+            <p class="card-text">Lingua originale:
+                <img class="w-25 m-2" :src="getImageUrl(movieInfo.original_language)" alt="">
+            </p>
             <p class="card-text">Voto medio: {{ movieInfo.vote_average }}</p>
         </div>
     </div>
@@ -15,7 +17,12 @@
         name: 'AppCard',
         props: {
             movieInfo: Object
-        }
+        },
+        methods: {
+            getImageUrl(name) {
+                return new URL(`../assets/img/flags/${name}.png`, import.meta.url).href;
+            },
+        },
     }
 </script>
 
@@ -26,5 +33,8 @@
             opacity: 1;
             transition: 1s;
         }
+    };
+    h5, p {
+        text-shadow: 1px 1px 1px black;
     }
 </style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="container d-flex flex-wrap justify-content-center gap-2 bg-dark p-5">
-        <AppCard v-for="movie in store.moviesArray" :movieInfo="movie"></AppCard>
+        <AppCard v-for="movie in store.moviesArray" :movieInfo="movie" :key="store.moviesArray.id"></AppCard>
     </div>
 </template>
 
